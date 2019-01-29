a8f75d24c4f9d1f664e810aa9b338bd9f1e3756b
Joaquin Jimenez <jjimenez@asmws.com>
2019-01-29 07:17:05 -0500

    cambios en master

--- src/Roca/Bundle/RefdataBundle/Entity/CountryTranslation.php
+++ src/Roca/Bundle/RefdataBundle/Entity/CountryTranslation.php
@@ -1,50 +1,4 @@
-<<<<<<< HEAD
 <?php
-
-namespace Roca\Bundle\RefdataBundle\Entity;
-
-use Akeneo\Component\Localization\Model\AbstractTranslation;
-use Akeneo\Component\Localization\Model\TranslationInterface;
-use Pim\Component\Catalog\Model\FamilyTranslationInterface;
-
-/**
- * CountryTranslation entity
- *
- * @author    J
- * @copyright 2017 ASMWS
- */
-class CountryTranslation extends AbstractTranslation implements TranslationInterface
-{
-    /** @var string */
-    protected $name;
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return (string) $this->getName();
-    }
-}
-=======
-<?php
-
 namespace Roca\Bundle\RefdataBundle\Entity;
 
 use Roca\Bundle\RefdataBundle\Entity\TranslatableCustomEntityInterface;
@@ -91,4 +45,3 @@ class CountryTranslation extends AbstractTranslation implements TranslationInter
         return (string) $this->label;
     }
 }
->>>>>>> jjd/cloudinary
